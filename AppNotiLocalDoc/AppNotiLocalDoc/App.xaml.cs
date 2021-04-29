aec8bd70a8b1d98b25291e74b010548ac0404cbb
NiditoPC <NiditoPC@DESKTOP-49UUCSP>
2021-04-29 14:51:36 -0500

    Agregar archivos de proyecto.

---
+++ AppNotiLocalDoc/AppNotiLocalDoc/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppNotiLocalDoc
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Get<INotificationManager>().Initialize();
+
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
